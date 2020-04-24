d71a4d824d93f1cb3e67dd4f8ad11f5a9e683d88
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-04-24 06:49:08 -0300

    Componentes Filhos I

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import Primeiro, { Segundo } from './component'
-
+//import Primeiro, { Segundo } from './component'
+import SilvaFamily from './silvaFamily'
 
 ReactDOM.render(
-    <div>
-        <Primeiro msg="é galerinha"/>
-        <Segundo msg="Tomamo no cu"/>
-    </div>
+    <SilvaFamily/>
+    // <div>
+    //     <Primeiro msg="é galerinha"/>
+    //     <Segundo msg="Tomamo no cu"/>
+    // </div>
 , document.getElementById('app'))
