2ccb1e876241075ce7e08e470f7f1a160d932f05
Gabriel Martins Padoin <gabrielmpwork@gmail.com>
2020-04-26 05:08:42 -0300

    Ex 01 - Integrando o React-Redux (Parte 1)

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-//import Primeiro, { Segundo } from './component'
-//import SilvaFamily from './silvaFamily'
-// import Family from './family'
-// import Member from './member'
-// import Componente from './classComponent'
+
 import Field from './field'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
@@ -17,15 +13,4 @@ ReactDOM.render(
     <Provider store={createStore(reducers)}>
         <Field initialValue="mano"/>
     </Provider>
-    // <Componente label="Contador" initialValue={10}/>
-    // <Family lastName="Josias">
-    //     <Member name="Jorge"/>
-    //     <Member name="Julia"/>
-    //     <Member name="Rafael"/>
-    // </Family>
-    //<SilvaFamily/>
-    // <div>
-    //     <Primeiro msg="é galerinha"/>
-    //     <Segundo msg="Tomamo no cu"/>
-    // </div>
 , document.getElementById('app'))
